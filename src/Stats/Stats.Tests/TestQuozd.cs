fca5f75046dff84651c80ea0c8383b0742e87961
Andrei Ignat <ignatandrei@yahoo.com>
2025-02-08 15:31:21 +0200

    https://github.com/quozd/awesome-dotnet/

---
+++ src/Stats/Stats.Tests/TestQuozd.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Stats.Tests;
+[TestClass]
+public class TestQuozd
+{
+    [TestMethod]
+    public async Task TestDeserialize()
+    {
+        var data = await File.ReadAllTextAsync(Path.Combine("data", "quozd.md"));
+        var s = new StatsServiceQuozd(new HttpClient());
+        var projects = await s.GetProjectsAsyncFromString(data).ToArrayAsync();
+        Assert.AreEqual(projects.Length, 757);
+        var one = projects.FirstOrDefault(it => it.Name == "OneOf");
+        Assert.IsNotNull(one);
+    }
+
+}
