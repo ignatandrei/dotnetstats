a4ce9595c6e40c89f891bf49c0621a12fb81773b
Andrei Ignat <ignatandrei@yahoo.com>
2025-02-10 07:59:07 +0200

    refactoring to display year and load one time

---
+++ src/Stats/StatsBlazorUI/AllData.cs
@@ -0,0 +1,18 @@
+﻿using StatsInterfaces.UI;
+
+namespace StatsBlazorUI;
+public class AllDataProjectsWithStars
+{
+    private List<IProjectWithStars> projectWithStarsData = [];
+    public bool FinishStreaming { get; set; }
+    public IProjectWithStars[] projectWithStars() {
+        return projectWithStarsData.ToArray();
+    }
+    public int Length => projectWithStarsData.Count;
+    public void AddProjectWithStars(params IProjectWithStars[] items)
+    {
+        projectWithStarsData.AddRange(items);
+    }
+
+
+}
